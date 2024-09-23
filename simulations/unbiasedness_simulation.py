bc24a4e9b98d5a39b4c97328251b6c48dff2e0b7
Francisco Sesto <sestofrancisco@gmail.com>
2024-09-23 16:06:55 -0300

    create template_dashboard

--- simulations/unbiasedness_simulation.py
+++ simulations/unbiasedness_simulation.py
@@ -10,122 +10,25 @@ import statsmodels.api as sm
 import os
 import pprint
 
+from template_dashboard import (
+    get_external_stylesheets,
+    style,
+    title_style,
+    COMPONENT_HEIGHT,
+    create_menu,
+    create_graph,
+    paper,
+    update_layout,
+    main_structure,
+    build_layout,
+)
 
 ######## CONFIGURACIÓN DE ESTILO  ##############
 
 dash._dash_renderer._set_react_version("18.2.0")
 
 
-# Configuración de estilo
-def get_external_stylesheets():
-    _VOLT_BASE = "https://cdn.jsdelivr.net/gh/stevej2608/volt-bootstrap-5-dashboard@1.4.2/dist/css/"
-    VOLT = _VOLT_BASE + "volt.min.css"
-    return [
-        "https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css",
-        "https://cdnjs.cloudflare.com/ajax/libs/chartist/0.11.4/chartist.min.css",
-        "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.2.0/css/all.min.css",
-        "https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.css",
-        VOLT,
-    ]
-
-
-style = {
-    # "height": 100,
-    "width": "100%",
-    # "border": f"1px solid {dmc.DEFAULT_THEME['colors']['indigo'][4]}",
-    "marginTop": 20,
-    "marginBottom": 20,
-}
-
-# Definir un estilo común para los títulos
-title_style = {
-    "marginBottom": 0,
-    "marginLeft": 40,
-    "marginTop": 20,
-    "textAlign": "left",
-}
-
-
-COMPONENT_HEIGHT = "400px"
-
-####### FUNCIONES #############
-
-
-# Create Menu
-def create_menu(font_weight=600, *args: dict) -> dmc.Accordion:
-    """
-    Create a Dash Mantine Accordion menu with multiple sections.
-
-    This function generates an accordion menu where each section consists of a title and content.
-    The content is rendered using Markdown with LaTeX math support enabled.
-
-    Parameters:
-    -----------
-    font_weight : int, optional
-        Font weight for the section titles in the accordion (default is 600).
-    *args : dict
-        Variable number of dictionaries representing each section of the accordion.
-        Each dictionary should have a single key-value pair:
-        - key (str): The title of the section
-        - value (str): The content of the section in Markdown format
-
-    Returns:
-    --------
-    dmc.Accordion
-        A Dash Mantine Accordion component with the specified sections and content.
-
-    Examples:
-    ---------
-    >>> section_1 = {"Introduction": "This is the **introduction** to the menu."}
-    >>> section_2 = {"Math Section": "Here is some math: $E=mc^2$."}
-    >>> menu = create_menu(500, section_1, section_2)
-    >>>
-    >>> # The returned 'menu' can then be used as part of a Dash layout:
-    >>> app.layout = dmc.Container(menu)
-
-    Notes:
-    ------
-    - The function uses `dmc.Accordion` and `dcc.Markdown` components from the Dash Mantine library.
-    - LaTeX math support is enabled in the Markdown content using `mathjax=True`.
-    - The accordion is configured with `disableChevronRotation=True`.
-
-    See Also:
-    ---------
-    dash_mantine_components.Accordion : For more details on the Accordion component.
-    dash_core_components.Markdown : For more information on Markdown rendering in Dash.
-    """
-    children_accordion = []
-    for dict_a in args:
-        title_section = list(dict_a.keys())[0]
-        content_section = list(dict_a.values())[0]
-        children_accordion.append(
-            dmc.AccordionItem(
-                [
-                    # Titulo
-                    dmc.AccordionControl(dmc.Text(title_section, fw=font_weight)),
-                    # Contenido
-                    dmc.AccordionPanel(dcc.Markdown(content_section, mathjax=True)),
-                ],
-                value=title_section,
-            ),
-        )
-    accordion = dmc.Accordion(disableChevronRotation=True, children=children_accordion)
-    return accordion
-
-
-# Función par crear gráfico
-def create_graph(id, title):
-    return dmc.Stack(
-        [
-            dmc.Text(title, fw=800),
-            dcc.Graph(id=id, style={"height": "100%", "width": "100%"}),
-        ],
-        style={"height": "100%"},
-        justify="flex-start",
-        gap="xs",
-    )
-
-
+############### FUNCIONES PROPIAS DE ESTA SIMULACIÓN ################
 # Función para crear la tabla de estadísticas
 def create_stats_table(stats, true_value, parameter_name):
     return dmc.Table(
@@ -200,42 +103,6 @@ def create_scatter_plot(X, Y):
     )
 
 
-# Actualizar gráfico y fig
-def update_layout(fig):
-    fig.update_layout(
-        height=300,
-        margin=dict(l=35, r=35, t=30, b=30),
-        modebar_remove=[
-            "select",
-            "lasso",
-            "zoomIn",
-            "zoomOut",
-            "autoScale",
-            "hover",
-            "toggleSpikelines",
-            "hoverClosestCartesian",
-            "hoverCompareCartesian",
-        ],
-    )
-    return fig
-
-
-def paper(content, height=COMPONENT_HEIGHT):
-    # fmt: off
-    return dmc.Paper(
-            children=[content],
-            p="xl",
-            shadow="xs",
-            withBorder=True,
-            style={
-                "height": height,
-                "borderRadius": "10px",
-                # "backgroundColor": "#f8f9fa",
-            },
-        )
-    # fmt: on
-
-
 def calculate_stats(beta_0_list, beta_1_list):
     return {
         "mean_beta_0": np.mean(beta_0_list),
@@ -243,87 +110,6 @@ def calculate_stats(beta_0_list, beta_1_list):
         "mean_beta_1": np.mean(beta_1_list),
         "var_beta_1": np.var(beta_1_list),
     }
-
-
-def main_structure(menu, structure: list[list]) -> dmc.Stack:
-    """
-    Create a Dash Mantine layout with a menu and a grid-based structure.
-
-    This function generates a layout where a menu is placed at the top, followed by
-    a grid structure with rows. Each row consists of exactly two columns, and each
-    column contains one or more Dash Mantine components.
-
-    Parameters:
-    -----------
-    menu : dmc.Component
-        The component representing the menu to be displayed at the top of the layout.
-    structure : list[list]
-        A list of lists where each sublist represents a row containing exactly two
-        columns. Each column is a list of one or more Dash Mantine components to
-        display within that column.
-
-    Returns:
-    --------
-    dmc.Stack
-        A Dash Mantine Stack component that contains the menu and a grid-based layout
-        with the specified rows and columns.
-
-    Examples:
-    ---------
-    >>> structure = [
-    >>>    [[component1, component2], [component3]],  # First row with two columns
-    >>>    [[component4], [component5, component6]]   # Second row with two columns
-    >>> ]
-    >>> layout = main_structure(menu_component, structure)
-    >>>
-    >>> # The returned 'layout' can then be used as part of a Dash layout:
-    >>> app.layout = dmc.Container(layout)
-
-    Notes:
-    ------
-    - Each row must contain exactly two columns, and each column can contain
-      one or more components.
-    - The function uses `dmc.Grid` and `dmc.GridCol` from the Dash Mantine library.
-    - A `ValueError` is raised if any row does not contain exactly two columns.
-
-    See Also:
-    ---------
-    dash_mantine_components.Grid : For more details on the Grid component.
-    dash_mantine_components.GridCol : For more information on how columns are handled.
-    """
-
-    content_grids = []
-
-    # Verificar que cada fila tenga exactamente dos elementos y que esos elementos sean listas
-    for fila in structure:
-        if len(fila) != 2:
-            raise ValueError("Cada fila debe contener exactamente dos elementos.")
-        if not all(isinstance(element, list) for element in fila):
-            raise ValueError("Cada elemento dentro de una fila debe ser una lista.")
-
-        # Crear las columnas (GridCol) de la fila
-        col_grids = [dmc.GridCol(children=element, span=6) for element in fila]
-
-        # Crear el grid de la fila
-        grid = dmc.Grid(
-            children=col_grids,
-            gutter="md",
-            align="stretch",
-        )
-
-        content_grids.append(grid)
-
-    # Crear la estructura principal con el menú y las filas
-    main_structure = dmc.Stack(
-        children=[
-            menu,  # Menú en la parte superior
-            *content_grids,  # Filas con sus respectivas columnas
-        ],
-        gap="lg",
-    )
-    pprint.pp(main_structure)
-
-    return main_structure
 
 
 ############### INSTANCIA DE LA APP ###############3
@@ -399,7 +185,7 @@ markdown_description = create_menu(
 ############ 2) GRÁFICOS ##############3
 
 # scatter
-scatter_content = create_graph("scatter", "Gráfico de dispersión")
+scatter_content = create_graph(id="scatter", title="Gráfico de dispersión")
 
 # histogram of beta 0
 histogram_beta_0_content = create_graph(
@@ -518,23 +304,11 @@ def update_histogram(error_std, x_std):
     return fig_histogram_0, fig_histogram_1, fig_scatter, beta_0_table, beta_1_table
 
 
-app.layout = dmc.MantineProvider(
-    theme={"colorScheme": "light"},
-    children=[
-        dmc.Container(
-            fluid=True,
-            # size="lg",
-            style=style,
-            children=[
-                dmc.Title(
-                    "Simulación OLS: Insesgadez y Varianza de Estimadores", order=2
-                ),
-                dmc.Space(h=20),
-                resumen_content,
-            ],
-        )
-    ],
+app.layout = build_layout(
+    title="Simulación OLS: Insesgadez y Varianza de Estimadores",
+    content=resumen_content,
 )
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("DASH_PORT"))
